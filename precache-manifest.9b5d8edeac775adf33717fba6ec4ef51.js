2f23497886e7d512c193a26d07931e7c9a85348d
yash <yashodip9@gmail.com>
2020-09-16 11:56:54 +0530

    Updates

---
+++ precache-manifest.9b5d8edeac775adf33717fba6ec4ef51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17ece292ac44ca917397780d2c24863",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "5b9977558c2cd1a10025",
+    "url": "/home/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b78bc67ec0f5d4ddab4a",
+    "url": "/home/static/css/main.1fb07f66.chunk.css"
+  },
+  {
+    "revision": "5b9977558c2cd1a10025",
+    "url": "/home/static/js/2.1ef0ff7a.chunk.js"
+  },
+  {
+    "revision": "25847a435ebc55fbc7da5bf3410fad16",
+    "url": "/home/static/js/2.1ef0ff7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78bc67ec0f5d4ddab4a",
+    "url": "/home/static/js/main.48383455.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "a765ead8d0d47ddbd838a57655c86afd",
+    "url": "/home/static/media/resume.a765ead8.pdf"
+  }
+]);
